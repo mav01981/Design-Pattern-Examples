6dd56414d3be226abfb3ded30841cfa6e0455c9a
Jonathan Smart <JonathanSmart@ChessICT.co.uk>
2018-05-30 17:12:23 +0100

    Added Observer Pattern

---
+++ Observer-Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using Observer;
+using System;
+
+namespace Observer_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TemperatureReporter provider = new TemperatureReporter();
+            TemperatureMonitor monitor_Indoors= new TemperatureMonitor();
+            TemperatureMonitor monitor_Outdoors = new TemperatureMonitor();
+
+            monitor_Outdoors.Subscribe(provider);
+            monitor_Outdoors.GetTemperature();
+
+            Console.Read();
+        }
+    }
+}
